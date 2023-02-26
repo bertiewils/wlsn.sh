ab81982bd12b97ee5771f884495f689875ec8a7f
Bertie Wilson <19255166+bertiewils@users.noreply.github.com>
2023-02-26 11:46:49 +0000

    Handle article titles

--- new_post.sh
+++ new_post.sh
@@ -5,11 +5,13 @@ set -e
 # Make sure we're in the root dir
 cd "$(git rev-parse --show-toplevel)"
 
-POST_SLUG="$1"
+POST_SLUG="$*"
 if [ -z "$POST_SLUG" ]; then
   read -rp "Post Name (e.g. your-new-post): " POST_SLUG
 fi
 TIMESTAMP=$(date +%Y-%m-%d)
+POST_SLUG=${POST_SLUG,,}  # Convert to owercase
+POST_SLUG=${POST_SLUG// /-}  # Replace spaces with -
 POST_FILENAME="${TIMESTAMP}-${POST_SLUG}.md"
 
 hugo new "content/posts/${POST_FILENAME}"
